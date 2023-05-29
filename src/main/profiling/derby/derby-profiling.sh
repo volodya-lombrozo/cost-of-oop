b2713deeae56ed884790b97da95cf7b26ec54dc9
volodya-lombrozo <volodya.lombrozo@gmail.com>
2023-05-29 14:33:13 +0300

    Move test environment variables upper

--- src/main/profiling/derby/derby-profiling.sh
+++ src/main/profiling/derby/derby-profiling.sh
@@ -1,51 +1,47 @@
 set -x
 set -e
 
+JMETER_PATH="/Users/lombrozo/Workspace/Tools/apache-jmeter-5.5/bin"
+JMETER_PLAN="Apache Derby.jmx"
+PROFILER_API="/Applications/YourKit-Java-Profiler-2022.9.app/Contents/Resources/lib/yjp-controller-api-redist.jar"
+PROFILER_AGENT="/Applications/YourKit-Java-Profiler-2022.9.app/Contents/Resources/bin/mac/libyjpagent.dylib"
+PROFILER_SNAPSHOTS="/Users/lombrozo/Snapshots"
+APPLICATION_JAR="/Users/lombrozo/Workspace/Tools/db-derby-10.16.1.1-bin/lib/derbyrun.jar server start"
+
 # In order to attach java agent use the next command for Mac
 # -agentpath:<profiler directory>/bin/mac/libyjpagent.dylib
 # To check commands for other OSs, please, visit https://www.yourkit.com/docs/java/help/agent.jsp
 # In my case <profiler directory> is /Applications/YourKit-Java-Profiler-2022.9.app/Contents/Resources/bin/mac/libyjpagent.dylib
 
 #Start derby
-java -agentpath:/Applications/YourKit-Java-Profiler-2022.9.app/Contents/Resources/bin/mac/libyjpagent.dylib -jar /Users/lombrozo/Workspace/Tools/db-derby-10.16.1.1-bin/lib/derbyrun.jar server start &
+java -agentpath:$PROFILER_AGENT -jar $APPLICATION_JAR &
 APP_PID=$!
 # Wait until startup
-
 sleep 5
-
 # Add load through JMeter
 # In my case JMeter path /Users/lombrozo/Workspace/Tools/apache-jmeter-5.5/bin
 # The full documentation about JMeter cli you can find right here:
 # https://jmeter.apache.org/usermanual/get-started.html#non_gui
-/Users/lombrozo/Workspace/Tools/apache-jmeter-5.5/bin/jmeter -n -t "Apache Derby.jmx" &
+$JMETER_PATH/jmeter -n -t "$JMETER_PLAN" &
 JMETER_PID=$!
-
 # Warmup
-
 sleep 5
-
 #Profile an application
-PROFILER=/Applications/YourKit-Java-Profiler-2022.9.app/Contents/Resources/lib/yjp-controller-api-redist.jar
-SNAPSHOTS=/Users/lombrozo/Snapshots
-java -jar $PROFILER status
-
-java -jar $PROFILER clear-cpu-data
-java -jar $PROFILER clear-alloc-data
-java -jar $PROFILER clear-monitor-data
-java -jar $PROFILER clear-charts
-
-java -jar $PROFILER start-tracing
+java -jar $PROFILER_API status
+java -jar $PROFILER_API clear-cpu-data
+java -jar $PROFILER_API clear-alloc-data
+java -jar $PROFILER_API clear-monitor-data
+java -jar $PROFILER_API clear-charts
+java -jar $PROFILER_API start-tracing
 echo "Profiling is started successfully"
 sleep 60
-java -jar $PROFILER capture-performance-snapshot
-java -jar $PROFILER stop-cpu-profiling
-ls -Artlh $SNAPSHOTS | tail -n 2
+java -jar $PROFILER_API capture-performance-snapshot
+java -jar $PROFILER_API stop-cpu-profiling
 echo  "Profiling is stopped successfully"
-
-
+#Show results
+ls -Artlh "$PROFILER_SNAPSHOTS" | tail -n 1
 #Stop load testing
-/Users/lombrozo/Workspace/Tools/apache-jmeter-5.5/bin/stoptest.sh
+$JMETER_PATH/stoptest.sh
 kill $JMETER_PID
-
 #Stop application
 kill $APP_PID
