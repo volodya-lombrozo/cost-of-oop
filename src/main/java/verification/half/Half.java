66a33dec1a9fb8ed0c265fe80568aee577047b06
volodya-lombrozo <volodya.lombrozo@gmail.com>
2023-06-01 17:57:33 +0300

    Add verification applications

---
+++ src/main/java/verification/half/Half.java
@@ -0,0 +1,37 @@
+package verification.half;
+
+public class Half {
+
+    public static void main(final String[] args) throws InterruptedException {
+        int sum = 0;
+        for (int i = 0; i < Integer.MAX_VALUE; ++i) {
+            Book b = new Discounted(i);
+            sum += Half.discounted(b.price());
+            Thread.sleep(10);
+        }
+        System.out.printf("Total: %d\n", sum);
+    }
+
+    interface Book {
+        int price();
+
+    }
+
+    private static int discounted(final int u) {
+        return u / 2;
+    }
+
+    static class Discounted implements Book {
+        private final int usd;
+
+        Discounted(final int u) {
+            this.usd = u;
+        }
+
+        @Override
+        public int price() {
+            return this.usd / 2;
+        }
+    }
+
+}
