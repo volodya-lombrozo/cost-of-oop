a67be1fb60dafd432730d4846041b2d3d4e7fa52
volodya-lombrozo <volodya.lombrozo@gmail.com>
2023-06-09 16:12:51 +0300

    Update struts-profiling script

--- src/main/profiling/struts/struts-profiling.sh
+++ src/main/profiling/struts/struts-profiling.sh
@@ -1,7 +1,5 @@
 export JMETER_PLAN="Struts-MVC.jmx"
-export JETTY_HOME="/Users/lombrozo/Workspace/Tools/jetty-distribution-9.4.51.v20230217"
-export JETTY_BASE="/Users/lombrozo/Workspace/Tools/jetty-distribution-9.4.51.v20230217"
-export APPLICATION_JAR="$JETTY_HOME/start.jar"
-export PROFILER_AGENT="/Applications/YourKit-Java-Profiler-2022.9.app/Contents/Resources/bin/mac/libyjpagent.dylib"
-export APPLICATION_STARTUP="cd $JETTY_HOME ; java -agentpath:$PROFILER_AGENT -jar $APPLICATION_JAR"
+export JETTY_HOME="/Users/lombrozo/Workspace/Tools/jetty-home-10.0.15"
+export JETTY_BASE="/Users/lombrozo/Workspace/Tools/jetty-home-10.0.15/base"
+export APPLICATION_JAR="$JETTY_HOME/start.jar jetty.base=$JETTY_BASE jetty.home=$JETTY_HOME"
 ../jar-profiling.sh
