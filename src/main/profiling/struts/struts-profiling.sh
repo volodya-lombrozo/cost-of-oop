2815dd4500c53be58301e137fe5abfed61291c13
volodya-lombrozo <volodya.lombrozo@gmail.com>
2023-06-09 15:15:25 +0300

    Try to run jetty from other folder

--- src/main/profiling/struts/struts-profiling.sh
+++ src/main/profiling/struts/struts-profiling.sh
@@ -1,3 +1,7 @@
 export JMETER_PLAN="Struts-MVC.jmx"
-export APPLICATION_JAR="/Users/lombrozo/Workspace/Tools/jetty-distribution-9.4.51.v20230217/start.jar"
+export JETTY_HOME="/Users/lombrozo/Workspace/Tools/jetty-distribution-9.4.51.v20230217"
+export JETTY_BASE="/Users/lombrozo/Workspace/Tools/jetty-distribution-9.4.51.v20230217"
+export APPLICATION_JAR="$JETTY_HOME/start.jar"
+export PROFILER_AGENT="/Applications/YourKit-Java-Profiler-2022.9.app/Contents/Resources/bin/mac/libyjpagent.dylib"
+export APPLICATION_STARTUP="cd $JETTY_HOME ; java -agentpath:$PROFILER_AGENT -jar $APPLICATION_JAR"
 ../jar-profiling.sh
