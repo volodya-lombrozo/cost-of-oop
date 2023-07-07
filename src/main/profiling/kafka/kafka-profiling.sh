5d179c13a4bcaf054056bdf1cd7d140215273fc9
volodya-lombrozo <volodya.lombrozo@gmail.com>
2023-07-07 14:10:52 +0300

    Add =_instrument_all_methods param for all the applications that we are going to profile

--- src/main/profiling/kafka/kafka-profiling.sh
+++ src/main/profiling/kafka/kafka-profiling.sh
@@ -3,7 +3,7 @@ export KAFKA_PATH=/Users/lombrozo/Workspace/Tools/apache-kafka
 $KAFKA_PATH/bin/zookeeper-server-start.sh $KAFKA_PATH/config/zookeeper.properties &
 sleep 10
 export PROFILER_AGENT="/Applications/YourKit-Java-Profiler-2022.9.app/Contents/Resources/bin/mac/libyjpagent.dylib"
-export KAFKA_OPTS="-agentpath:$PROFILER_AGENT"
+export KAFKA_OPTS="-agentpath:$PROFILER_AGENT=_instrument_all_methods"
 export APPLICATION_STARTUP="$KAFKA_PATH/bin/kafka-server-start.sh $KAFKA_PATH/config/server.properties"
 ../jar-profiling.sh
 $KAFKA_PATH/bin/kafka-server-stop.sh
